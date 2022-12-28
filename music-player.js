8a3f92a71411b470960d780fbf1182a3f727df09
Rowida <rowidanagah462000@gmail.com>
2022-12-28 21:14:54 +0200

    show a playlist form ur music folder

--- music-player.js
+++ music-player.js
@@ -1,12 +1,10 @@
 
 audio = document.getElementById("audio-player");
 audioSource = document.getElementById("audioSource");
-
 lst = ["canon.mp3", "dream.mp3", "hero.mp3", "mombasa.mp3", "swan-lake.mp3"]
 min = 0;
 max = lst.length
 songsContainer = document.getElementById("songs")
-
 
 
 /*
@@ -28,9 +26,8 @@ function repeat(){
 function creatSongsLsr() {
     songsContainer.innerHTML = ""
     lst.forEach((ele, indx) => {
-        console.log("INDX& ELE", ele, indx);        
         songsContainer.innerHTML += `
-        <li><button class="btn btn-secondary option col-6"><a data-value="AudioSamples/${ele}">${ele}</a>
+        <li><button onclick="playSong(${indx})" class="btn btn-secondary option col-6"><a data-value="AudioSamples/${ele}">${ele}</a>
         <a onclick="deleteSong(${indx})" > <i class='fa fa-remove'></i></button></a> </button>
         </li>`
     });
@@ -38,15 +35,26 @@ function creatSongsLsr() {
 
 function deleteSong(indx){
     lst.splice(indx, 1); /**delet a spesific */
+    max = lst.length /**keepon tracking the len of the updated lst */
     creatSongsLsr() /** recreat song list */
 
 }
 
+function playSong(indx){
+    /**of a spesific indx */
+    audio.src = `AudioSamples/${lst[indx]}`;
+    console.log(`AudioSamples/${lst[indx]}`, audio.src)
+    play();
+    
+}
+
 function Repeat() {
-    reff = audioSource.src
-    ref = reff.split("/")
+    // reff = audioSource.src
+    // ref = reff.split("/")
+    console.log(audioSource)
     audio.src = `AudioSamples/${ref[ref.length - 1]}`
-    audio.play();
+    console.log(audio)
+    play();
 
 }
 
@@ -59,13 +67,12 @@ function shuffle() {
     indx = Math.floor(Math.random() * (max - min) + min);
     audio.src = `AudioSamples/${lst[indx]}`;
     console.log(`AudioSamples/${lst[indx]}`, audio.src)
-    audio.play();
+    play();
 }
 
 function pause() {
     audio.pause();
 }
-
 
 function nextaudio() {
     /**grap source of things */
@@ -74,8 +81,8 @@ function nextaudio() {
     indx = lst.indexOf(ref[ref.length - 1]) + 1
     console.log(ref[ref.length - 1], indx % max, `AudioSamples/${lst[indx % max]}`)
     audio.src = `AudioSamples/${lst[indx % max]}`;
-    console.log(audio.src)
-    audio.play();
+    console.log(audio.src ,indx % max )
+    play();
 }
 
 function prvaudio() {
@@ -87,8 +94,8 @@ function prvaudio() {
     console.log("index  " + idx)
     console.log("src is", `AudioSamples/${lst[idx]}`)
     audio.src = `AudioSamples/${lst[idx]}`;
-    console.log(audio.src)
-    audio.play();
+    console.log(audio.src ,indx % max )
+    play();
 }
 
 /*list.onclick = function(e) {
@@ -110,32 +117,26 @@ audiofile = document.getElementById('al-add');
 var blob = window.URL || window.webkitURL;
 
 /**upload audio */
-audiofile.addEventListener('change', function (target) {
-
-    console.log('change on input#file triggered');
-    var file = target.files[0]
-    urlObj = URL.createObjectURL(this.files[0])
-    console.log( "url is   ---- " ,urlObj);
+audiofile.addEventListener('change', function (e) {
+    var file = e.target.files[0]
+    console.log( '-----------------' , file);
 
     fileURL = blob.createObjectURL(file);
-    lst.push(fileURL);
+    lst.push(file.name);
     creatSongsLsr();
-    console.log('File BlobURL: ' + fileURL);
-    console.log(URL.revokeObjectURL(urlObj))
     audio.src = fileURL;
+    audioSource.src = fileURL 
     console.log(audio)
-    audio.play()
+    play()
 });
 
 /*play from lst */
 list.onclick = function (e) {
     audio = document.getElementById("audio-player");
-    console.log(audio)
     e.preventDefault();
-
     var element = e.target;
     audio.src = element.getAttribute('data-value');
-
+    console.log(audio)
     audio.load(); //call this to just preload the audio without playing
     audio.play();
 };
